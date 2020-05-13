af12ea9b6e936996e283d77244204965433a7fa9
Christopher Peers <christopher_peers@outlook.com>
2020-05-13 10:29:59 +0100

    Initial css resetting and addition of LinearCard.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import React from "react"
 
+import LinearCard from '../components/linear-card';
+import indexStyles from './index.module.scss';
+
 const IndexPage = () => {
 
     return (
-        <div>
-            <h1>Roche Leigh Construction</h1>
+        <div className={ indexStyles.wrapper }>
+            <LinearCard>
+                <h1 className={ indexStyles.title }>Roche Leigh Construction</h1>
+            </LinearCard>
         </div>
     )
 }
