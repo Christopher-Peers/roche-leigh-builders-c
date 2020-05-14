060d8ad5d806ae4ff9886bf529fef43dbc77dcf4
Christopher Peers <christopher_peers@outlook.com>
2020-05-14 21:04:21 +0100

    Adds an SVG hamburger icon.

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -6,7 +6,13 @@ const NavBar = () => {
 
     return (
         <div className={ navBarStyles.navBarWrapper }>
-            <button className={ navBarStyles.hamburger }>Click</button>
+            <button className={ navBarStyles.hamburger }>
+                <svg viewBox="0 0 100 80" width="22" height="40">
+                    <rect width="100" height="8" rx="4"></rect>
+                    <rect y="30" width="100" height="8" rx="4"></rect>
+                    <rect y="60" width="100" height="8" rx="4"></rect>
+                </svg>
+            </button>
         </div>
     );
 }
