73bfe192aa3bc4296b28c933e43a26aef2cf9993
Christopher Peers <christopher_peers@outlook.com>
2020-05-17 14:36:13 +0100

    Creates initial part of navigation for Carousel.

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -1,5 +1,8 @@
 import React, { useRef, useState, useLayoutEffect } from 'react';
 
+import { FaAngleLeft, FaAngleRight } from 'react-icons/fa';
+
+
 import carouselStyles from '../styles/carousel.module.scss';
 
 const Carousel = () => {
@@ -19,9 +22,11 @@ const Carousel = () => {
 	return (
 		<div ref={ targetRef } className={ carouselStyles.carouselContainer }>
 			<div className={ carouselStyles.slides }>
+				<FaAngleLeft className={ carouselStyles.leftAngle } size="2rem"/>
 				<div className={ carouselStyles.slideLeft }></div>
 				<div ref={ centerSlideRef } className={ carouselStyles.slideCenter }></div>
 				<div className={ carouselStyles.slideRight }></div>
+				<FaAngleRight className={ carouselStyles.rightAngle } size="2rem"/>
 			</div>
 			<div className={ carouselStyles.indicatorTrack }>
 				<div className={ carouselStyles.indicator }></div>
@@ -30,6 +35,13 @@ const Carousel = () => {
 			</div>
 		</div>
 	);
+
+	// Dynamic width with max setting
+	// Initial setting of left for left and center slides, right for right slide
+	// Re-draw for left and right click of the carousel buttons
+	// Redraw on resize for desktop browsers or orientation change on mobile devices
+
+
 
 }
 
