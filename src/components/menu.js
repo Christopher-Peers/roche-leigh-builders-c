bd4eb31f052f84f7c10453e2f52958fd945f5621
Christopher Peers <christopher_peers@outlook.com>
2020-06-06 20:06:52 +0100

    Adds links and styling to menu.

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,20 +1,32 @@
 import React from 'react';
 
+import { Link } from 'gatsby'
+
 import menuStyles from '../styles/menu.module.scss';
 const Menu = ({ isOpen }) => {
 
     return (
         <div className={ `${menuStyles.menu} ${ isOpen ? menuStyles.menuOpen : '' }` }>
             <div className={ menuStyles.menuCloseWrapper}>
-
+                <button className={ menuStyles.closeButton } >X</button>
             </div>
 
-            <div className={ menuStyles.menuLinkWrapper }>
-                <p>Home</p>
-                <p>Services</p>
-                <p>Recent Work</p>
-                <p>Contact</p>
-            </div>
+            <nav className={ menuStyles.menuLinkWrapper }>
+                <ul className={ menuStyles.menuLinks }>
+                    <li className={ menuStyles.menuLink }>
+                        <Link to="/">About us</Link>
+                    </li>
+                    <li className={ menuStyles.menuLink }>
+                        <Link to="/">Our Services</Link>
+                    </li>
+                    <li className={ menuStyles.menuLink }>
+                        <Link to="/">Contact us</Link>
+                    </li>
+                    <li className={ menuStyles.menuLink }>
+                        <Link to="/">Testimonials</Link>
+                    </li>
+                </ul>
+            </nav>
 
         </div>
     );
